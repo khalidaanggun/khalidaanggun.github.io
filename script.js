3d58fa12f0194824b16ab508c2405a0402f441c5
Khalida <khalida@gmail.com>
2019-10-15 14:09:11 +0200

    push

---
+++ script.js
@@ -0,0 +1,114 @@
+/*console.log('Hello World!')
+var firstname = 'John';
+console.log(firstname);
+
+
+var lastname= 'Smith';
+var ageJohn = 25;
+
+var iki= 'iki';
+var ikisage= 25;
+var fullAge = true;
+console.log(iki);
+console.log(fullAge);
+
+var job, isMarried;
+job= 'Teacher';
+isMarried= false;
+
+
+console.log(firstname+''+lastname+''+ageJohn+ ''+ job+ ''+ isMarried);
+
+alert(firstname+''+lastname+''+ageJohn+ ''+ job+ ''+ isMarried);
+prompt('what is his last nmae?');
+console.log(firstname+''+ lastname);
+
+
+//type coersion= int/number is converted as string or other data type depends on what is needed
+
+var year, johnYear, markYear;
+now= 2019
+yearJohn= now- ageJohn;
+yeariki = now-ikisage;
+console.log(yeariki);
+
+
+console.log(now+ 2)
+
+console.log(now/2)
+
+var johnOlder= ageJohn > ikisage;
+
+var johnOlder= ageJohn == ikisage;
+console.log(johnOlder);
+
+//typeof operator
+console.log(typeof johnOlder);
+console.log(typeof ageJohn);
+console.log(typeof 'Mark is older than John');
+var x;
+
+console.log(typeof x);
+ 
+var ikisheight, ikisweight, matisheight, matisweight, bmiIki , bmiMati;
+
+
+ikisheight=  prompt('insert height in meters');
+//matisheight=  prompt('insert height');
+console.log(ikisheight+' ' +'in meter');
+
+ikisweight=  prompt('insert weight in kg');
+//matisweight=  prompt('insert weight');
+console.log(ikisweight + ' '+'in kg'+' ' );
+
+
+function square(x)
+{
+    return (x*x);
+}
+
+
+/*alert('BMI Mati is'+  ' ' + (ikisweight/(square(ikisheight) )));
+function alert
+{
+    
+    
+}*/
+
+
+for (var i= 0 ; i<10; i++)
+    {
+        console.log(i);
+        
+    }
+
+/*function factorial (z,y)
+{
+    if(z==1|| z==0)
+    {
+    console.log(1);    
+    }
+    else
+    {
+        for (y=1; y=(z-1); y++)
+            {
+                console.log(z*(z-y));
+            }
+    }
+  
+}*/
+
+function factorial (n){
+  if (n==0 || n==1){
+    return 1;
+  }
+  return factorial(n-1)*n;
+} 
+
+console.log(factorial(5));
+
+console.log('halid')
+
+ = function document.getElementById("myButton").onclick() {
+        location.href = "https://www.youtube.com/watch?v=74iHoCM83Mg";
+    }
